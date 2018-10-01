7fe9ff74f71bd2220aade6f35273c2fa41d52433
keitmark <keitmark@mail.ru>
2018-10-01 12:28:25 +0300

    home page done

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,47 @@ $(document).ready(function () {
 //        vertical:true
 //    });
     
-
     $('.column-projects').masonry({
       itemSelector: '.column-projects__item'
-
     });
+    
+    
+    $('.ham-menu').click(function(){
+        $('.xs-menu').addClass('showed');
+    });
+    
+    $('.xs-menu__close').click(function(){
+        $('.xs-menu').removeClass('showed');
+    });
+    
+    
+    var win = $(window);
+    
+
+    var scrFunc = function () {
+        var t = win.scrollTop(),
+            e = win.height();
+        $("[data-anim], .column-projects__item").each(function (n, i) {
+            var r = $(i).offset().top,
+                s = t + .9 * e;
+            s > r ? $(i).attr("data-anim", "true") : true;
+
+        });
+    }
+    
+    scrFunc();
+    
+    $(window).scroll(function(){
+         scrFunc();
+        
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
 });
